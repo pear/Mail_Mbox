55c25a1164d4af89ecd351469b4e7aeb9dbac3a1
Roberto Berto <darkelder@php.net>
2002-12-30 16:24:18 +0000

    Release 0.1.4
    git-svn-id: https://svn.php.net/repository/pear/packages/Mail_Mbox/trunk@109412 c90b9560-bf6c-de11-be94-00142212c4b1

--- mbox.php
+++ mbox.php
@@ -74,6 +74,87 @@ require_once "PEAR.php";
     *   Use it to parse headers and body.
     *   http://pear.php.net/package-info.php?pacid=21
     *
+    * EXAMPLE: 
+    // some random content
+    $content = <<<EOF
+From Foo@example.com Fri Dec 27 14:31:10 2002
+Return-Path: 
+Received: from [unix socket] by campos.example.com (LMTP); Fri, 27 Dec
+    2002 14:31:10 -0200 (BRST)
+Date: Fri, 27 Dec 2002 14:31:21 -0500
+Message-Id: <200212271931.gBRJVL012289@example.com>
+Received: from  pcp128525pcs.foo.example.com (
+    pcp128525pcs.example.com [99.99.99.99]) by/
+    serjolen6com.example.com (v64.19) with ESMTP id
+    MAILRELAYINZA98-3601058302; Fri, 08 Nov 2002 06:39:05 -0500
+From: "Foo@example.com"
+To: fool@example.com
+Subject: This is A SPAM!!
+Content-Type: text/plan
+
+testing foo spam
+EOF;
+
+    // starting mbox
+    require_once "mbox.php";
+    $mbox    =      new Mail_Mbox();
+
+    // uncomment to see lots of things
+    #$mbox->debug    = true;
+
+    // opennign file mbox
+    $mid     =     $mbox->open("mbox");
+
+    // uncomment to see internal vars
+    #print_r($mbox);
+
+
+
+    // deleting a message (uncomment to test)
+    #$res1 =  $mbox->remove($mid,0);
+    if (PEAR::isError($res1))
+    {
+        print $res1->getMessage();
+    }
+
+
+
+
+
+        // changing a message (uncomment to test)
+    #$res2 = $mbox->update($mid,0,$content);
+        if (PEAR::isError($res2))
+        {
+                print $res2->getMessage();
+        }
+
+
+        // adding a message (uncomment to test)
+        $res3 = $mbox->insert($mid,$content,0);
+        if (PEAR::isError($res3))
+        {
+                print $res3->getMessage();
+        }
+
+
+
+    require_once "Mail/mimeDecode.php";
+    // showing current messages with Mail Mime
+    for ($x = 0; $x < $mbox->size($mid); $x++)
+    {
+        printf("Message: %08d<pre>",$x);
+        $thisMessage     = $mbox->get($mid,$x);
+        print $thisMessage;    
+        print "<hr />";
+        $decode = new Mail_mimeDecode($thisMessage, "\r\n");
+        $structure = $decode->decode();
+        print_r($structure);
+
+        print "</pre><hr /><hr /><hr />";
+    }
+    *
+    *
+    *
     * @author   Roberto Berto <darkelder@php.net>
     * @package  Mail
     * @access   public
@@ -506,5 +587,5 @@ class Mail_Mbox extends PEAR
     
     }    
 }
-
+  
 ?>
