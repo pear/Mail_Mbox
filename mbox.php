d525e779b6d42ed24a7f0caac65912c5cef9430a
Roberto Berto <darkelder@php.net>
2004-10-07 14:04:55 +0000

    removed lgpl license
    git-svn-id: https://svn.php.net/repository/pear/packages/Mail_Mbox/trunk@170030 c90b9560-bf6c-de11-be94-00142212c4b1

--- mbox.php
+++ mbox.php
@@ -23,23 +23,6 @@ require_once "PEAR.php";
     /**
     * Mbox PHP class to Unix MBOX parsing and using
     * 
-    * LICENSE (LGPL)
-    * Copyright (C) 2002-2003 Roberto Berto
-    * This library is free software; you can redistribute it and/or
-    * modify it under the terms of the GNU Lesser General Public
-    * License as published by the Free Software Foundation; either
-    * version 2.1 of the License, or (at your option) any later version.
-    *
-    * This library is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    * Lesser General Public License for more details.
-    *
-    * You should have received a copy of the GNU Lesser General Public
-    * License along with this library; if not, write to the Free Software
-    * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-    * Or at http://www.gnu.org/licenses/lgpl.txt
-    *
     * 
     * METHODS:
     * int resource mbox->open(string file)
